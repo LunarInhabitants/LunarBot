eedd86e305d2f815e46f53161506a47db5ab6867
danm36 <dan36masterson@hotmail.com>
2017-03-28 00:07:31 +0100

    Initial Commit of Lunar Bot and Example Plugin Bot has basic command support via delegates Bot has basic plugin support Bot can reload plugins at runtime

---
+++ ExamplePlugin/Plugin.cs
@@ -0,0 +1,30 @@
+﻿using Discord;
+using LunarBotRuntime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[Serializable]
+public class Example : LunarBotPlugin
+{
+    public Example()
+    {
+        Name = "Test Plugin that was reloaded";
+        Description = "This is a test of the plugin system";
+        Author = "Daniel Masterson";
+    }
+
+    public override void OnPluginLoad()
+    {
+        CommandManager.AddCommand("plugintest", async (e) =>
+        {
+            e.Channel.SendMessage("This plugin dun did werk and was reloaded");
+        });
+    }
+
+    public override void OnPluginUnload()
+    {
+    }
+}
---
+++ LunarBotRuntime/LunarBotPlugin.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LunarBotRuntime
+{
+    [Serializable]
+    public abstract class LunarBotPlugin : MarshalByRefObject
+    {
+        public string Name { get; protected set; }
+        public string Description { get; protected set; }
+        public string Author { get; protected set; }
+
+        public abstract void OnPluginLoad();
+        public abstract void OnPluginUnload();
+
+        internal string GetPluginDescription()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"**{Name}** by *{Author}*");
+            sb.AppendLine(Description);
+            return sb.ToString();
+        }
+    }
+}
